7fbf121abf144bdd00d90132e49ebc2b7e60e22a
hiwanglf <hiwanglf@163.com>
2018-02-27 15:25:14 +0800

    Revert "提交代码"
    This reverts commit 8f3190f4d6028bfdd08a8c18601b751380392add.

--- funny_001_最小公倍数.py
+++
@@ -1,7 +0,0 @@
-a = 3
-b = 5
-m = max(a, b)
-for x in range(m, a*b+1):
-    if x%a == 0 and x%b== 0:
-        print(x)
-        break
--- funny_002_最大公约数.py
+++
@@ -1,7 +0,0 @@
-a = 3
-b = 5
-m = min(a, b)
-for x in range(m,0,-1):
-    if a%x ==0 and b%x==0:
-        print(x)
-        break
